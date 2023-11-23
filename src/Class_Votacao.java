62708fcc3e84942e2b501b06f945b3c0aeb76889
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-11-23 15:43:27 -0300

    Sistema de votacao simples em Java

---
+++ src/Class_Votacao.java
@@ -0,0 +1,45 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Class_Votacao {
+    public static void main(String[] args) {
+
+        System.out.println("******************************");
+        System.out.println("\nVotacao!");
+        System.out.println("\n******************************");
+
+        String menu = """
+                Escolha Dentre os Numeros Listados o seu voto.
+                
+                Numeros = 123, 323, 321.
+                
+                9 - Sair
+                """;
+
+        int contadorNum123 = 0;
+        int contadorNum323 = 0;
+        int contadorNum321 = 0;
+        int numeroEscolhido = 0;
+        Scanner leitura = new Scanner(System.in);
+
+        while(numeroEscolhido != 9){
+            System.out.println(menu);
+            numeroEscolhido = leitura.nextInt();
+
+            if(numeroEscolhido == 123){
+                contadorNum123++;
+            } else if(numeroEscolhido ==323){
+                contadorNum323++;
+            } else if (numeroEscolhido == 321){
+                contadorNum321++;
+            } else if( numeroEscolhido != 9){
+                System.out.println("Numero invalido");
+            }
+        }
+
+        System.out.println("\n#### Total de Votos #### " );
+        System.out.println("\nNumero 123: " + contadorNum123);
+        System.out.println("Numero 323: " + contadorNum323);
+        System.out.println("Numero 321: " + contadorNum321);
+    }
+}
